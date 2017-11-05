500bc17959858a1eda3046545f7db12be6bb2919
Allie Grampa <agrampa@yahoo.com>
2017-11-05 12:40:24 -0800

    Update article.js

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -26,7 +26,7 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-Article.loadAll = rows => {
+Article.loadAll = rawData => {
   rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   // TODO: Refactor this .forEach() code, by using a .map() call instead, since what we are trying to accomplish is the transformation of one collection into another. Remember that we can set variables equal to the result of functions. So if we set a variable equal to the result of a .map(), it will be our transformed array.
